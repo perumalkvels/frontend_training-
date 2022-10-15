507673cffb1124148448e6376f90ba64e12791dc
perumalkvels <perumal.k159@gmail.com>
2022-10-14 22:27:40 -0700

    comorins Food Project Ok

--- Oct_10_22/Comorins/Food_App_Project/js/user.js
+++ Oct_10_22/Comorins/Food_App_Project/js/user.js
@@ -1,26 +1,31 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const name = urlParams.get('name');
-let elementa;
-let loginStatusa=JSON.parse(localStorage.getItem("login_status"));
-if(name==loginStatusa.username || loginStatusa.username!=""){
-elementa="<a class='nav-link'  href='#' ><span class='mr-3 h4 text-white p-0'>hi !</span><span class='text-warning h4' >"+loginStatusa.username+"</span><button class='badge user-logout badge-pill ml-3 badge-warning'onclick='userLogout()'><span class='p-4 text-center'>Logout<span></button></a> ";
-document.getElementById("user_status").innerHTML=elementa;
+const userName = urlParams.get('name');
 
+let element;
+let loginStatus = JSON.parse(localStorage.getItem("login_status"));
+if (userName == loginStatus.username || loginStatus.username != "") {
+    element = "<a class='nav-link'  href='#' ><span class='mr-3 h4 text-white p-0'>hi !</span><span class='text-warning h4' >" + loginStatus.username + "</span><button class='badge user-logout badge-pill ml-3 badge-warning'onclick='userLogout()'><span class='p-4 text-center'>Logout<span></button></a> ";
+    document.getElementById("user_status").innerHTML = element;
 }
 
-function userLogout(){
+// Function Define for User Login
+
+function userLogout() {
     localStorage.removeItem("login_status");
+    localStorage.removeItem("Order_Items");
     document.location.reload(true);
 }
 
-function isUserAlive(){
-// alert("hai");
-if(!localStorage.getItem("login_status")){
-            alert("Please Login to Enter Cart");
-        }
-        else{
-            
-            window.location.href = "productCart.html";
-        }
+// Function Define for Check Is User is Available
+
+function isUserAlive() {
+    if (!localStorage.getItem("login_status")) {
+        alert("Please Login to Enter Cart");
+        window.location.href = "login.html";
+    }
+    else {
+
+        window.location.href = "productCart.html";
+    }
 }
